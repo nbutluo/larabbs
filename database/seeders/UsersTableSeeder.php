e7889a1ac261e4b5a98dd3cd77bb02851bda0f05
nbutluo <nbutluo@163.com>
2021-04-26 14:09:19 +0800

    填充用户和话题数据

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 生成数据集合
+        User::factory()->count(10)->create();
+
+        // 单独处理第一个用户
+        $user = User::find(1);
+        $user->name = 'nbutluo';
+        $user->email = 'nbutluo@163.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
