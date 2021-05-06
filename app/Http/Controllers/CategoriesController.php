4c70620ed12b3c1329dd2b5d89127371301eef94
nbutluo <nbutluo@163.com>
2021-05-06 10:59:00 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,10 +8,13 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
         // 读取分类ID关联的话题
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+        $topics = $topic->withOrder($request->order)
+            ->with('user', 'category')
+            ->where('category_id', $category->id)
+            ->paginate();
         // 传参变量话题和分类到视图中
         return view('topics.index', compact('topics', 'category'));
     }
