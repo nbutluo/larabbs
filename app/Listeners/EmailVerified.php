6fff414b41d08cc154167b919515b1b060e45067
nbutluo <nbutluo@163.com>
2022-02-24 17:22:26 +0800

    认证后的提示

---
+++ app/Listeners/EmailVerified.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class EmailVerified
+{
+    public function handle(Verified $event)
+    {
+        session()->flash('success', '邮箱验证成功^_^');
+    }
+}
